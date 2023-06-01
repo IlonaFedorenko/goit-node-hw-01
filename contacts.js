c54fc27f9b086ecfdde9bdc5d0e56ef5dc0f552c
IlonaFedorenko <86719203+IlonaFedorenko@users.noreply.github.com>
2023-06-01 21:42:23 +0200

    fix

--- contacts.js
+++ contacts.js
@@ -25,7 +25,16 @@ const addContact = async (data) => {
   return newContact;
 };
 
-const removeContact = async (contactId) => {};
+const removeContact = async (id) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
 
 module.exports = {
   listContacts,
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const fs = require("fs").promises;
 const path = require("path");
 const contactsPath = path.join("db", "contacts.json");
-const { nanoid } = require("nanoid");
 
 const {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
+  removeContact,
 } = require("./contacts");
 
 async function invokeAction({ action, id, name, email, phone }) {
@@ -27,20 +26,26 @@ async function invokeAction({ action, id, name, email, phone }) {
       console.table(newContact);
       break;
 
-    // case "remove":
-
-    //   break;
+    case "remove":
+      const deletedContact = await removeContact(id);
+      console.table(deletedContact);
+      break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
 
+
 // invokeAction({ action: "list" });
 // invokeAction({ action: "get", id: "drsAJ4SHPYqZeG-83QTVW" });
+// invokeAction({
+//   action: "add",
+//   name: "Mango",
+//   email: "mango@gmail.com",
+//   phone: "322-22-22",
+// });
 invokeAction({
-  action: "add",
-  name: "Mango",
-  email: "mango@gmail.com",
-  phone: "322-22-22",
+  action: "remove",
+  id: "fgJHK38jqZx_r31_GZGRp",
 });
